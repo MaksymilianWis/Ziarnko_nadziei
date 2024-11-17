39bb8f18d5e430576fc85dd28aa5146790bb3b39
DLUGI <272549@student.pwr.edu.pl>
2024-11-17 14:41:35 +0100

    korekta

--- test.m
+++ test.m
@@ -1,3 +1,4 @@
+clear all;
 %unzip("rice.zip");
 
 data = imageDatastore("rice", IncludeSubfolders = true, LabelSource = "foldernames");
@@ -6,3 +7,8 @@ classNames = categories(data.Labels);
 
 [dataTrain, DataValidation, dataTest, dataPlay] = splitEachLabel(data, 0.7, 0.14, 0.15, 0.01);
 
+layers = [
+    imageInputLayer([250 250 1])
+
+    convolution2Layer(5, 64, 'Padding','same')
+]
